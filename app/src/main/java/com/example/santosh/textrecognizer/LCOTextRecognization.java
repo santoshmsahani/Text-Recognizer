dabb6b4350389cdad68b5e510dee688149041810
santoshsahani <santoshsahani48@gmail.com>
2018-12-27 12:37:52 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/santosh/textrecognizer/LCOTextRecognization.java
@@ -0,0 +1,15 @@
+package com.example.santosh.textrecognizer;
+
+import android.app.Application;
+
+import com.google.firebase.FirebaseApp;
+
+public class LCOTextRecognization extends Application {
+    public static final String RESULT_TEXT="RESULT_TEXT";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FirebaseApp.initializeApp(this);
+    }
+}
---
+++ app/src/main/java/com/example/santosh/textrecognizer/ResultActivity.java
@@ -0,0 +1,31 @@
+package com.example.santosh.textrecognizer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ResultActivity extends AppCompatActivity {
+
+    TextView resultTextView;
+    Button backButton;
+    String resultText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        resultTextView=findViewById(R.id.result_textview);
+        backButton=findViewById(R.id.back_button);
+        resultText=getIntent().getStringExtra(LCOTextRecognization.RESULT_TEXT);
+
+        resultTextView.setText(resultText);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
